992f2ea5a6f52a5507e0acfa8284641ca19aa28b
Mayuresh Kamble <mackam30@gmail.com>
2026-01-06 00:21:47 +0530

    added header

--- Memory_Game/src/App.jsx
+++ Memory_Game/src/App.jsx
@@ -1,12 +1,12 @@
+import { GameHeader } from "../components/GameHeader"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
+  <div className="app">
+    <GameHeader score={6} moves={9}/>
+  </div>
   )
 }
 
 export default App
+ 
